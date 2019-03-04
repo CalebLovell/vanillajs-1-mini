4ffc82f1f0d05bae865664547b0f576e7b7206be
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-03-04 11:39:48 -0700

    step 5

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('Hey idiot')
+console.log('Hey idiot');
+
+let count = 0;
+const element = document.getElementById('counter')
+
+function increase() {
+    count++;
+    element.innerText = count
+    console.log(`Increased by ${count}`);
+}
+
+function decrease() {
+    count--;
+    element.innerText = count
+    console.log("Decreased by", count);
+}
+
+function reset() {
+    count = 0;
+    element.innerHTML = "<mark>" + count + "</mark>"
+    console.log("Reset to", count);
+}
+
+function selectTheme(theme) {
+    document.getElementsByTagName('body')[0].className = theme;
+    document.getElementsByTagName('main')[0].className = theme;
+    const buttons = document.getElementsByTagName('button');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].className = theme;
+    }
+}
